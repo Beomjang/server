e57535fac9b983ce1cacb952037f51155c005167
Beomjang <ibjang05@naver.com>
2025-06-23 16:22:59 +0900

    Add /about route

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 3000;  // Render는 환경변수 PORT를 씀
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+  res.send('달려라장인범123');
+});
+
+app.get('/about', (req, res) => {
+  res.send('달려라장인범123');
 });
 
 app.listen(PORT, () => {
